767bb3f2cb9824df71b70b9f89bfcaa8c734fb7b
Chris Smart <mail@christophersmart.com>
2009-10-14 12:15:17 +1100

    Fixed layer issue on manifest installer icon.

--- dmenu.sh
+++ dmenu.sh
@@ -3,8 +3,9 @@
 #This script requires the dmenu package from the wmii window manager. Install it with your distro's package management tool.
 #You can then put this script somewhere and add a shortcut key in your desktop environment to call it.
 
-#To exclude anything from the PATH (like perl modules, etc) then add another variable and then add to PATHS.
-NOPERL="sed s/[a-zA-Z0-9\/]*perl[a-zA-Z0-9\/]*//g"
+#dsTo exclude anything from the PATH (like perl modules, etc) then add another variable and then add to PATHS.
+
+ERL="sed s/[a-zA-Z0-9\/]*perl[a-zA-Z0-9\/]*//g"
 
 PATHS="`echo $PATH |sed 's/:/ /g' |$NOPERL`"
 
